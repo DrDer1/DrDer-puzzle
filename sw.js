3acc50266bf07f59e5911f84d776836fe48806cb
abdalaziz babker <abdalazizlaptop@gmail.com>
2026-07-16 23:07:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'v2';
+var CACHE_VERSION = 'v3';
 var CACHE_NAME = 'drder-puzzle-' + CACHE_VERSION;
 var ASSETS = [
   './',
@@ -6,39 +6,34 @@ var ASSETS = [
   './style.css',
   './app.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './192.png',
+  './512.png'
 ];
 
 self.addEventListener('install', function(event) {
   console.log('SW: Installing ' + CACHE_NAME);
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      console.log('SW: Caching ' + ASSETS.length + ' files');
       return cache.addAll(ASSETS).catch(function(err) {
         console.warn('SW: Some files failed to cache', err);
       });
     }).then(function() {
-      console.log('SW: Skip waiting');
       return self.skipWaiting();
     })
   );
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('SW: Activating');
   event.waitUntil(
     caches.keys().then(function(names) {
       return Promise.all(
         names.filter(function(name) {
           return name.startsWith('drder-puzzle-') && name !== CACHE_NAME;
         }).map(function(name) {
-          console.log('SW: Deleting old cache:', name);
           return caches.delete(name);
         })
       );
     }).then(function() {
-      console.log('SW: Claiming clients');
       return self.clients.claim();
     })
   );
@@ -58,7 +53,7 @@ self.addEventListener('fetch', function(event) {
           var clone = response.clone();
           caches.open(CACHE_NAME).then(function(cache) {
             cache.put(event.request, clone);
-          }).catch(function() {});
+          });
         }
         return response;
       }).catch(function() {
